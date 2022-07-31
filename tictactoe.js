03e825a8f6267d3bd58ec42c7905f820fe9469d8
Md Shadab Ahamad <73027972+mdshadab998998@users.noreply.github.com>
2022-07-31 19:44:00 +0530

    Add files via upload

---
+++ tictactoe.js
@@ -0,0 +1,536 @@
+// var restartb= document.querySelector("#b")
+// var cells= document.querySelectorAll("td")
+// var Xcounter= document.getElementById("X")
+// var Ocounter= document.getElementById("O")
+// var date=document.getElementById("date")
+// date.style.color=("green")
+
+
+
+// function clearAllCell(){
+//     for(var i=0; i<cells.length; i++)
+//     {
+//     cells[i].textContent=""
+// }
+// }
+
+
+
+// restartb.addEventListener("click", clearAllCell)
+// function ChangeContent(){
+//     if(this.textContent==""){
+//         this.textContent="X"
+
+//     }
+       
+// }
+
+
+
+// for(var i=0; i<cells.length;i++)
+// {
+//     cells[i].addEventListener("click", ChangeContent)
+    
+// }
+
+
+
+
+// function reply_click(clicked_id)
+// {
+// var r=clicked_id
+// if(r==1){
+//     if(cells[1].textContent==""){
+//         cells[1].textContent="O"
+//     }
+//     else if(cells[2].textContent==""){
+//         cells[2].textContent="O"
+//     }
+//     else if(cells[3].textContent==""){
+//         cells[3].textContent="O"
+//     }
+//     else if(cells[4].textContent==""){
+//         cells[4].textContent="O"
+//     }
+//     else if(cells[5].textContent==""){
+//         cells[5].textContent="O"
+//     }
+//     else if(cells[6].textContent==""){
+//         cells[6].textContent="O"
+//     }
+//     else if(cells[7].textContent==""){
+//         cells[7].textContent="O"
+//     }
+//     else if(cells[8].textContent==""){
+//         cells[8].textContent="O"
+//     }
+// }
+// if(r==2){
+//     if(cells[0].textContent==""){
+//         cells[0].textContent="O"
+//     }
+//     else if(cells[2].textContent==""){
+//         cells[2].textContent="O"
+//     }
+//     else if(cells[3].textContent==""){
+//         cells[3].textContent="O"
+//     }
+//     else if(cells[4].textContent==""){
+//         cells[4].textContent="O"
+//     }
+//     else if(cells[5].textContent==""){
+//         cells[5].textContent="O"
+//     }
+//     else if(cells[6].textContent==""){
+//         cells[6].textContent="O"
+//     }
+//     else if(cells[7].textContent==""){
+//         cells[7].textContent="O"
+//     }
+//     else if(cells[8].textContent==""){
+//         cells[8].textContent="O"
+//     }
+// }
+// if(r==3){
+//     if(cells[1].textContent==""){
+//         cells[1].textContent="O"
+//     }
+//     else if(cells[0].textContent==""){
+//         cells[0].textContent="O"
+//     }
+//     else if(cells[3].textContent==""){
+//         cells[3].textContent="O"
+//     }
+//     else if(cells[4].textContent==""){
+//         cells[4].textContent="O"
+//     }
+//     else if(cells[5].textContent==""){
+//         cells[5].textContent="O"
+//     }
+//     else if(cells[6].textContent==""){
+//         cells[6].textContent="O"
+//     }
+//     else if(cells[7].textContent==""){
+//         cells[7].textContent="O"
+//     }
+//     else if(cells[8].textContent==""){
+//         cells[8].textContent="O"
+//     }
+// }
+// if(r==4){
+//     if(cells[1].textContent==""){
+//         cells[1].textContent="O"
+//     }
+//     else if(cells[2].textContent==""){
+//         cells[2].textContent="O"
+//     }
+//     else if(cells[0].textContent==""){
+//         cells[0].textContent="O"
+//     }
+//     else if(cells[4].textContent==""){
+//         cells[4].textContent="O"
+//     }
+//     else if(cells[5].textContent==""){
+//         cells[5].textContent="O"
+//     }
+//     else if(cells[6].textContent==""){
+//         cells[6].textContent="O"
+//     }
+//     else if(cells[7].textContent==""){
+//         cells[7].textContent="O"
+//     }
+//     else if(cells[8].textContent==""){
+//         cells[8].textContent="O"
+//     }
+// }
+// if(r==5){
+//     if(cells[1].textContent==""){
+//         cells[1].textContent="O"
+//     }
+//     else if(cells[2].textContent==""){
+//         cells[2].textContent="O"
+//     }
+//     else if(cells[3].textContent==""){
+//         cells[3].textContent="O"
+//     }
+//     else if(cells[0].textContent==""){
+//         cells[0].textContent="O"
+//     }
+//     else if(cells[5].textContent==""){
+//         cells[5].textContent="O"
+//     }
+//     else if(cells[6].textContent==""){
+//         cells[6].textContent="O"
+//     }
+//     else if(cells[7].textContent==""){
+//         cells[7].textContent="O"
+//     }
+//     else if(cells[8].textContent==""){
+//         cells[8].textContent="O"
+//     }
+// }
+// if(r==6){
+//     if(cells[1].textContent==""){
+//         cells[1].textContent="O"
+//     }
+//     else if(cells[2].textContent==""){
+//         cells[2].textContent="O"
+//     }
+//     else if(cells[3].textContent==""){
+//         cells[3].textContent="O"
+//     }
+//     else if(cells[4].textContent==""){
+//         cells[4].textContent="O"
+//     }
+//     else if(cells[0].textContent==""){
+//         cells[0].textContent="O"
+//     }
+//     else if(cells[6].textContent==""){
+//         cells[6].textContent="O"
+//     }
+//     else if(cells[7].textContent==""){
+//         cells[7].textContent="O"
+//     }
+//     else if(cells[8].textContent==""){
+//         cells[8].textContent="O"
+//     }
+// }
+// if(r==7){
+//     if(cells[1].textContent==""){
+//         cells[1].textContent="O"
+//     }
+//     else if(cells[2].textContent==""){
+//         cells[2].textContent="O"
+//     }
+//     else if(cells[3].textContent==""){
+//         cells[3].textContent="O"
+//     }
+//     else if(cells[4].textContent==""){
+//         cells[4].textContent="O"
+//     }
+//     else if(cells[5].textContent==""){
+//         cells[5].textContent="O"
+//     }
+//     else if(cells[0].textContent==""){
+//         cells[0].textContent="O"
+//     }
+//     else if(cells[7].textContent==""){
+//         cells[7].textContent="O"
+//     }
+//     else if(cells[8].textContent==""){
+//         cells[8].textContent="O"
+//     }
+// }
+// if(r==8){
+//     if(cells[1].textContent==""){
+//         cells[1].textContent="O"
+//     }
+//     else if(cells[2].textContent==""){
+//         cells[2].textContent="O"
+//     }
+//     else if(cells[3].textContent==""){
+//         cells[3].textContent="O"
+//     }
+//     else if(cells[4].textContent==""){
+//         cells[4].textContent="O"
+//     }
+//     else if(cells[5].textContent==""){
+//         cells[5].textContent="O"
+//     }
+//     else if(cells[6].textContent==""){
+//         cells[6].textContent="O"
+//     }
+//     else if(cells[0].textContent==""){
+//         cells[0].textContent="O"
+//     }
+//     else if(cells[8].textContent==""){
+//         cells[8].textContent="O"
+//     }
+// }
+// if(r==9){
+//     if(cells[1].textContent==""){
+//         cells[1].textContent="O"
+//     }
+//     else if(cells[2].textContent==""){
+//         cells[2].textContent="O"
+//     }
+//     else if(cells[3].textContent==""){
+//         cells[3].textContent="O"
+//     }
+//     else if(cells[4].textContent==""){
+//         cells[4].textContent="O"
+//     }
+//     else if(cells[5].textContent==""){
+//         cells[5].textContent="O"
+//     }
+//     else if(cells[6].textContent==""){
+//         cells[6].textContent="O"
+//     }
+//     else if(cells[7].textContent==""){
+//         cells[7].textContent="O"
+//     }
+//     else if(cells[0].textContent==""){
+//         cells[0].textContent="O"
+//     }
+// }
+// }
+
+// -------------------------------------------------------------------------------------
+function myfunc(){
+    var b1,b2,b3,b4,b5,b6,b7,b8,b9;
+    b1=document.getElementById("b1").innerHTML;
+    b2=document.getElementById("b2").innerHTML;
+    b3=document.getElementById("b3").innerHTML;
+    b4=document.getElementById("b4").innerHTML;
+    b5=document.getElementById("b5").innerHTML;
+    b6=document.getElementById("b6").innerHTML;
+    b7=document.getElementById("b7").innerHTML;
+    b8=document.getElementById("b8").innerHTML;
+    b9=document.getElementById("b9").innerHTML;
+    if(b1=="X" && b2=="X" && b3=="X"){
+        document.getElementById("banner").innerHTML="player X win"
+    }
+    else if(b1=="X" && b4=="X" && b7=="X"){
+        document.getElementById("banner").innerHTML="player X win"
+    }    
+    else if(b7=="X" && b8=="X" && b9=="X"){
+        document.getElementById("banner").innerHTML="player X win"
+    }    
+    else if(b9=="X" && b6=="X" && b3=="X"){
+        document.getElementById("banner").innerHTML="player X win"
+    }    
+    else if(b4=="X" && b5=="X" && b6=="X"){
+        document.getElementById("banner").innerHTML="player X win"
+    }    
+    else if(b2=="X" && b5=="X" && b8=="X"){
+        document.getElementById("banner").innerHTML="player X win"
+    }    
+    else if(b1=="X" && b5=="X" && b9=="X"){
+        document.getElementById("banner").innerHTML="player X win"
+    }    
+    else if(b3=="X" && b5=="X" && b7=="X"){
+        document.getElementById("banner").innerHTML="player X win"
+    }  
+    else if(b1=="O" && b2=="O" && b3=="O"){
+        document.getElementById("banner").innerHTML="player O win"
+    }
+    else if(b1=="O" && b4=="O" && b7=="O"){
+        document.getElementById("banner").innerHTML="player O win"
+    }    
+    else if(b7=="O" && b8=="O" && b9=="O"){
+        document.getElementById("banner").innerHTML="player O win"
+    }    
+    else if(b9=="O" && b6=="O" && b3=="O"){
+        document.getElementById("banner").innerHTML="player O win"
+    }    
+    else if(b4=="O" && b5=="O" && b6=="O"){
+        document.getElementById("banner").innerHTML="player O win"
+    }    
+    else if(b2=="O" && b5=="O" && b8=="O"){
+        document.getElementById("banner").innerHTML="player O win"
+    }    
+    else if(b1=="O" && b5=="O" && b9=="O"){
+        document.getElementById("banner").innerHTML="player O win"
+    }    
+    else if(b3=="O" && b5=="O" && b7=="O"){
+        document.getElementById("banner").innerHTML="player O win"
+    }  
+
+}
+bgx=document.getElementById("X")
+fgx=document.getElementById("X")
+bgo=document.getElementById("O")
+fgo=document.getElementById("O")
+
+bgx.style.backgroundColor="green"
+fgx.style.color="white"
+
+var flag=1;
+function myfunc1(){
+    if (flag==1){
+        document.getElementById("b1").innerHTML="X"
+        flag=0
+        bgo.style.backgroundColor="green"
+        fgo.style.color="white"
+        bgx.style.backgroundColor="whitesmoke"
+        fgx.style.color="black"
+    }
+    else{
+        document.getElementById("b1").innerHTML="O"
+        flag=1
+        bgx.style.backgroundColor="green"
+        fgx.style.color="white"
+        bgo.style.backgroundColor="whitesmoke"
+        fgo.style.color="black"
+
+
+    }
+}
+function myfunc2(){
+    if (flag==1){
+        document.getElementById("b2").innerHTML="X"
+        flag=0
+        bgo.style.backgroundColor="green"
+        fgo.style.color="white"
+        bgx.style.backgroundColor="whitesmoke"
+        fgx.style.color="black"
+    }
+    else{
+        document.getElementById("b2").innerHTML="O"
+        flag=1
+        bgx.style.backgroundColor="green"
+        fgx.style.color="white"
+        bgo.style.backgroundColor="whitesmoke"
+        fgo.style.color="black"
+
+    }
+}
+function myfunc3(){
+    if (flag==1){
+        document.getElementById("b3").innerHTML="X"
+        flag=0
+        bgo.style.backgroundColor="green"
+        fgo.style.color="white"
+        bgx.style.backgroundColor="whitesmoke"
+        fgx.style.color="black"
+    }
+    else{
+        document.getElementById("b3").innerHTML="O"
+        flag=1
+        bgx.style.backgroundColor="green"
+        fgx.style.color="white"
+        bgo.style.backgroundColor="whitesmoke"
+        fgo.style.color="black"
+
+    }
+}
+function myfunc4(){
+    if (flag==1){
+        document.getElementById("b4").innerHTML="X"
+        flag=0
+        bgo.style.backgroundColor="green"
+        fgo.style.color="white"
+        bgx.style.backgroundColor="whitesmoke"
+        fgx.style.color="black"
+    }
+    else{
+        document.getElementById("b4").innerHTML="O"
+        flag=1
+        bgx.style.backgroundColor="green"
+        fgx.style.color="white"
+        bgo.style.backgroundColor="whitesmoke"
+        fgo.style.color="black"
+
+    }
+}
+function myfunc5(){
+    if (flag==1){
+        document.getElementById("b5").innerHTML="X"
+        flag=0
+        bgo.style.backgroundColor="green"
+        fgo.style.color="white"
+        bgx.style.backgroundColor="whitesmoke"
+        fgx.style.color="black"
+    }
+    else{
+        document.getElementById("b5").innerHTML="O"
+        flag=1
+        bgx.style.backgroundColor="green"
+        fgx.style.color="white"
+        bgo.style.backgroundColor="whitesmoke"
+        fgo.style.color="black"
+
+    }
+}
+function myfunc6(){
+    if (flag==1){
+        document.getElementById("b6").innerHTML="X"
+        flag=0
+        bgo.style.backgroundColor="green"
+        fgo.style.color="white"
+        bgx.style.backgroundColor="whitesmoke"
+        fgx.style.color="black"
+    }
+    else{
+        document.getElementById("b6").innerHTML="O"
+        flag=1
+        bgx.style.backgroundColor="green"
+        fgx.style.color="white"
+        bgo.style.backgroundColor="whitesmoke"
+        fgo.style.color="black"
+
+    }
+}
+function myfunc7(){
+    if (flag==1){
+        document.getElementById("b7").innerHTML="X"
+        flag=0
+        bgo.style.backgroundColor="green"
+        fgo.style.color="white"
+        bgx.style.backgroundColor="whitesmoke"
+        fgx.style.color="black"
+    }
+    else{
+        document.getElementById("b7").innerHTML="O"
+        flag=1
+        bgx.style.backgroundColor="green"
+        fgx.style.color="white"
+        bgo.style.backgroundColor="whitesmoke"
+        fgo.style.color="black"
+
+    }
+}
+function myfunc8(){
+    if (flag==1){
+        document.getElementById("b8").innerHTML="X"
+        flag=0
+        bgo.style.backgroundColor="green"
+        fgo.style.color="white"
+        bgx.style.backgroundColor="whitesmoke"
+        fgx.style.color="black"
+    }
+    else{
+        document.getElementById("b8").innerHTML="O"
+        flag=1
+        bgx.style.backgroundColor="green"
+        fgx.style.color="white"
+        bgo.style.backgroundColor="whitesmoke"
+        fgo.style.color="black"
+
+    }
+}
+function myfunc9(){
+    if (flag==1){
+        document.getElementById("b9").innerHTML="X"
+        flag=0
+        bgo.style.backgroundColor="green"
+        fgo.style.color="white"
+        bgx.style.backgroundColor="whitesmoke"
+        fgx.style.color="black"
+    }
+    else{
+        document.getElementById("b9").innerHTML="O"
+        flag=1
+        bgx.style.backgroundColor="green"
+        fgx.style.color="white"
+        bgo.style.backgroundColor="whitesmoke"
+        fgo.style.color="black"
+
+    }
+}
+
+function myfunc_reset(){
+    location.reload()
+    // document.getElementById("b1").innerHTML=""
+    // document.getElementById("b2").innerHTML=""
+    // document.getElementById("b3").innerHTML=""
+    // document.getElementById("b4").innerHTML=""
+    // document.getElementById("b5").innerHTML=""
+    // document.getElementById("b6").innerHTML=""
+    // document.getElementById("b7").innerHTML=""
+    // document.getElementById("b8").innerHTML=""
+    // document.getElementById("b9").innerHTML=""
+    // document.getElementById("banner").innerHTML=""
+}
+
+
